da850438b3d1e5cdc40e058dfe5be053fe5f0d68
vvv <vvv@ss.com>
2020-07-22 02:02:06 +0800

    fix test script

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=1 python test_net.py --dataset pascal_voc --net vgg16 \
+CUDA_VISIBLE_DEVICES=1 python test_net.py --dataset pascal_voc --net res102 \
                     --checksession 1 --checkepoch 7 --checkpoint 10021 \
                     --cuda
