e76138b05e5c8023a9d4239bad5ec7c2f5af540c
jiruyi <18810981229@gmail.com>
2020-07-21 11:15:46 +0000

    change setting to pascal voc 2007 res101 75.2

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=0 python trainval_net.py \
-                   --dataset pascal_voc --net vgg16 \
+                   --dataset pascal_voc --net res101 \
                    --bs 1 --nw 8 --lr 0.001 --lr_decay_step 5 --epochs 12 \
                    --cuda
