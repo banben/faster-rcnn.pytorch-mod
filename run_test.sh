217c87836e31d9c8a9c3805af61e703629edd0f3
vvv <vvv@ss.com>
2020-07-22 01:57:26 +0800

    add test info

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,3 @@
-python test_net.py --dataset pascal_voc --net vgg16 \
-                    --checksession 1 --checkepoch 10 --checkpoint 10021 \
+CUDA_VISIBLE_DEVICES=0 python test_net.py --dataset pascal_voc --net vgg16 \
+                    --checksession 1 --checkepoch 6 --checkpoint 10021 \
                     --cuda
