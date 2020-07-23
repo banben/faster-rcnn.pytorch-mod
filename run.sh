59984cb6fd845abd39e45eeaeb1c562c0ae60757
vvv <vvv@ss.com>
2020-07-23 16:23:43 +0800

    change dataset to 2012

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=0 python trainval_net.py \
-                   --dataset pascal_voc --net res101 \
+                   --dataset pascal_voc_2012 --net res101 \
                    --bs 1 --nw 8 --lr 0.001 --lr_decay_step 5 --epochs 12 \
                    --cuda
